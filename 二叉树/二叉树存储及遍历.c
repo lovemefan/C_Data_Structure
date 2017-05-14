88294ba41815611dfa87df1f7278ca60f30578ce
Lovemefan <450489712@qq.com>
2017-05-14 10:34:32 +0800

    添加新函数
    先，中，后遍历。求单节点，双结点，叶子结点数量，求树的高度

--- 二叉树/二叉树存储及遍历.c
+++ 二叉树/二叉树存储及遍历.c
@@ -13,7 +13,11 @@ Status CreateBiTree(BiTree *T)
 {
 	//按顺序输入二叉树中结点的值 ，空格表示空树，构造二叉链表表示的二叉树T 
 	char c;
-	c=getchar();
+	do
+	{
+		c=getchar();
+	}
+	while(c==32 || c=='\n');//跳过空格或回车 
 	if(c=='#')
 		(*T)=NULL;
 	else
@@ -34,7 +38,7 @@ Status Visit(TElemType elem)
 	printf("%C ",elem);
 	return OK;
 }
-Status preOrderTraverse(BiTree T,Status(*Visit)(TElemType))
+Status PreOrderTraverse(BiTree T,Status(*Visit)(TElemType))
 {
 	//采用二叉链表储存结构，Visit 是对结点操作的函数
 	//先序遍历二叉树T，对每个结点调用函数Visit有且只有一次 
@@ -42,8 +46,8 @@ Status preOrderTraverse(BiTree T,Status(*Visit)(TElemType))
 	if(T)
 	{
 		if(Visit(T->data))
-			if(preOrderTraverse(T->lchild,Visit));
-				if(preOrderTraverse(T->rchild,Visit));
+			if(PreOrderTraverse(T->lchild,Visit));
+				if(PreOrderTraverse(T->rchild,Visit));
 		return OK;
 	}
 	else
@@ -56,9 +60,9 @@ Status InOrderTraverse(BiTree T,Status(*Visit)(TElemType))
 	//一旦Visit失败，则操作失败	
 	if(T)
 	{
-		if(preOrderTraverse(T->lchild,Visit));
+		if(InOrderTraverse(T->lchild,Visit));
 			if(Visit(T->data))
-				if(preOrderTraverse(T->rchild,Visit));
+				if(InOrderTraverse(T->rchild,Visit));
 		return OK;
 	}
 	else
@@ -71,8 +75,8 @@ Status PostOrderTraverse(BiTree T,Status(*Visit)(TElemType))
 	//一旦Visit失败，则操作失败	
 	if(T)
 	{
-		if(preOrderTraverse(T->lchild,Visit));
-			if(preOrderTraverse(T->rchild,Visit));
+		if(PostOrderTraverse(T->lchild,Visit));
+			if(PostOrderTraverse(T->rchild,Visit));
 				if(Visit(T->data))
 		return OK;
 	}
@@ -85,11 +89,125 @@ Status LevelOrderTraverse(BiTree T,Status(*Visit)(TElemType))
 	//层序遍历二叉树T，对每个结点调用函数Visit有且只有一次 
 	//一旦Visit失败，则操作失败	
 }
+int  LeafNodeNum(BiTree T)
+{
+//	求叶子结点的数量 
+	int n1,n2;//左右子树的叶子数 
+	if(!T)
+			return 0;
+	else
+	{
+		if(!T->lchild&&!T->rchild) 
+		{
+//			printf("%c是叶子\n",T->data);
+			return 1;
+		}
+		else
+		{
+			n1=LeafNodeNum(T->lchild);
+			n2=LeafNodeNum(T->rchild);
+			return n1+n2;
+		}
+	}
+
+	
+}
+int SingleNodeNum(BiTree T)
+{
+//	求单节点的数量 
+	int n1,n2;//左右子树的单节点 
+	if(!T)
+		return 0;
+	else
+	{
+		if(T->lchild&&!T->rchild || !T->lchild&&T->rchild)
+			return 1;
+		else
+		{
+			n1=SingleNodeNum(T->lchild);
+			n2=SingleNodeNum(T->rchild);
+			return n1+n2;
+		}
+	}
+	
+} 
+int DoubleNodeNum(BiTree T)
+{
+//	求双节点的数量 
+	int n1,n2;//左右子树的双节点 
+	if(!T)
+		return 0;
+	else
+	{
+		n1=SingleNodeNum(T->lchild);
+		n2=SingleNodeNum(T->rchild);
+		if(T->lchild&&T->rchild)
+		{	
+			return 1+n1+n2;
+		}
+		else
+		{
+			return n1+n2;
+		}
+	}
+	
+} 
+int TreeDepth(BiTree T)
+{
+//	求树的深度 
+	int n1,n2;//左右子数的高度
+	if(!T)
+		return 0;
+	else
+	{
+		n1=TreeDepth(T->lchild);
+		n2=TreeDepth(T->rchild);
+		return 1+(n1>n2?n1:n2);	//左右子数中最高的数加一 
+	} 
+	
+}
+int AllNodeNum(BiTree T)
+{
+//	求所有结点数
+	int n1,n2;//左右子树的结点数 
+	if(!T)
+	{
+		return  0;
+	} 
+	else
+	{
+		n1=AllNodeNum(T->lchild);
+		n2=AllNodeNum(T->rchild);
+		return 1+n1+n2;
+	}
+}
 int main()
 {
 	BiTree T;
-	printf("先序输入树，以空以#结束");
+	int n;
+	printf("先序输入树，以空以#结束\n");
 	CreateBiTree(&T);
-	preOrderTraverse(T,Visit); 
+	printf("\n先序遍历");
+	PreOrderTraverse(T,Visit); 
+	printf("\n中序遍历");
+	InOrderTraverse(T,Visit);
+	printf("\n后序遍历");
+	PostOrderTraverse(T,Visit);
+	
+	n=LeafNodeNum(T);
+	printf("\n叶子节点数 %d \n",n);
+	
+	n=SingleNodeNum(T);
+	printf("单节点数 %d \n",n);
+	
+	n=DoubleNodeNum(T);
+	printf("双节点数 %d \n",n);
+	
+	n=AllNodeNum(T);
+	printf("总结点数 %d \n",n);
+	
+	n=TreeDepth(T);
+	printf("树的深度 %d \n",n);
+	
 	return 0;
 }
