6630e4efee0c2e3fe52c2fded024b6e4c730fe28
Lovemefan <450489712@qq.com>
2017-05-15 12:09:53 +0800

    层序遍历还有bug，待修复

--- 二叉树/二叉树存储及遍历.c
+++ 二叉树/二叉树存储及遍历.c
@@ -2,13 +2,72 @@
 #include <stdlib.h> 
 #define OK 1
 #define ERROR 0
+#define TRUE 1
+#define FALSE 0
+#define OVERFLOW -1
 typedef int Status;
 typedef char TElemType;
-//----------------二叉树的二叉链表储存 ---------------- 
+//----------------------------------二叉树的二叉链表储存 -------------------------------------- 
 typedef struct Node{
 	TElemType data;
 	struct Node *lchild,*rchild;//左右孩子指针 
 }BiTNode,*BiTree;
+//----------------------------------队列链表储存结构----------------------------------------- 
+typedef struct qnode	/* 队列结点结构 */
+{
+   BiTNode elem;
+   struct qnode *next;
+}QNode,*QueuePtr;
+
+typedef struct			/* 队列的链表结构 */
+{
+   QueuePtr front,rear; /* 队头、队尾指针 */
+}LinkQueue;
+
+
+//---------------------------------------队列操作函数------------------------------------------
+Status InitQueue(LinkQueue *Q)
+{ 
+	Q->front=Q->rear=(QueuePtr)malloc(sizeof(QNode));
+	if(!Q->front)
+		exit(OVERFLOW);
+	Q->front->next=NULL;
+	return OK;
+}
+Status QueueEmpty(LinkQueue Q)
+{ 
+	if(Q.front==Q.rear)
+		return TRUE;
+	else
+		return FALSE;
+}
+Status EnQueue(LinkQueue *Q,BiTNode e)
+{ 
+	QueuePtr s=(QueuePtr)malloc(sizeof(QNode));
+	if(!s) /* 存储分配失败 */
+		exit(OVERFLOW);
+	s->elem=e;
+	s->next=NULL;
+	Q->rear->next=s;	/* 把拥有元素e的新结点s赋值给原队尾结点的后继，见图中① */
+	Q->rear=s;		/* 把当前的s设置为队尾结点，rear指向s，见图中② */
+	return OK;
+}
+
+/* 若队列不空,删除Q的队头元素,用e返回其值,并返回OK,否则返回ERROR */
+Status DeQueue(LinkQueue *Q,BiTNode *e)
+{
+	QueuePtr p;
+	if(Q->front==Q->rear)
+		return ERROR;
+	p=Q->front->next;		/* 将欲删除的队头结点暂存给p，见图中① */
+	*e=(p->elem);				/* 将欲删除的队头结点的值赋值给e */
+	Q->front->next=p->next;/* 将原队头结点的后继p->next赋值给头结点后继，见图中② */
+	if(Q->rear==p)		/* 若队头就是队尾，则删除后将rear指向头结点，见图中③ */
+		Q->rear=Q->front;
+	free(p);
+	return OK;
+}
+//---------------------------------------二叉树操作--------------------------------------------------------
 Status CreateBiTree(BiTree *T)
 {
 	//按顺序输入二叉树中结点的值 ，空格表示空树，构造二叉链表表示的二叉树T 
@@ -43,6 +102,11 @@ Status PreOrderTraverse(BiTree T,Status(*Visit)(TElemType))
 	//采用二叉链表储存结构，Visit 是对结点操作的函数
 	//先序遍历二叉树T，对每个结点调用函数Visit有且只有一次 
 	//一旦Visit失败，则操作失败	
+	//	先序遍历二叉树的操作定义：
+	//	若二叉树为空，则空操作；否则n  
+	//	1） 访问根结点
+	//	2） 先序遍历左子树
+	//	3） 先序遍历右子树
 	if(T)
 	{
 		if(Visit(T->data))
@@ -58,6 +122,11 @@ Status InOrderTraverse(BiTree T,Status(*Visit)(TElemType))
 	//采用二叉链表储存结构，Visit 是对结点操作的函数
 	//中遍历二叉树*T，对每个结点调用函数Visit有且只有一次 
 	//一旦Visit失败，则操作失败	
+	//中序遍历二叉树的操作定义：
+	//	若二叉树为空，则空操作；否则
+	//	1） 中序遍历左子树
+	//	2） 访问根结点
+	//	3） 中序遍历右子树
 	if(T)
 	{
 		if(InOrderTraverse(T->lchild,Visit));
@@ -73,6 +142,11 @@ Status PostOrderTraverse(BiTree T,Status(*Visit)(TElemType))
 	//采用二叉链表储存结构，Visit 是对结点操作的函数
 	//后序遍历二叉树*T，对每个结点调用函数Visit有且只有一次 
 	//一旦Visit失败，则操作失败	
+	//后序遍历二叉树的操作定义：
+	//	若二叉树为空，则空操作；否则
+	//	1） 后序遍历左子树
+	//	2） 后序遍历右子树
+	//	3） 访问根结点
 	if(T)
 	{
 		if(PostOrderTraverse(T->lchild,Visit));
@@ -88,6 +162,38 @@ Status LevelOrderTraverse(BiTree T,Status(*Visit)(TElemType))
 	//采用二叉链表储存结构，Visit 是对结点操作的函数
 	//层序遍历二叉树T，对每个结点调用函数Visit有且只有一次 
 	//一旦Visit失败，则操作失败	
+	//	层次遍历二叉树的操作定义：
+	//	若二叉树为空，则空操作；否则
+	//	1） 从上到下
+	//	2） 同一层，从左到右依次遍历
+	LinkQueue LQ;
+	InitQueue(&LQ);
+	BiTNode p;//接收出队列的结点
+	if(T)
+	{
+		EnQueue(&LQ,*T);
+//		printf("%c",LQ.front->data->data);	
+		while(!QueueEmpty(LQ))
+		{
+			DeQueue(&LQ,&p);
+	
+			printf("%c ",p.data);
+			if(p.lchild!=NULL)
+			{
+				EnQueue(&LQ,*(p.lchild));
+				printf("%c进队列 ",(p.lchild)->data);
+			}
+				
+			if(p.lchild!=NULL)
+			{
+				EnQueue(&LQ,*(p.rchild));
+				printf("%c进队列 ",(p.rchild)->data);
+			}
+				
+		}
+		
+	} 
+	return OK; 
 }
 int  LeafNodeNum(BiTree T)
 {
@@ -185,6 +291,9 @@ int main()
 {
 	BiTree T;
 	int n;
+	BiTNode p;
+	LinkQueue LQ;
+	InitQueue(&LQ);
 	printf("先序输入树，以空以#结束\n");
 	CreateBiTree(&T);
 	printf("\n先序遍历");
@@ -193,6 +302,13 @@ int main()
 	InOrderTraverse(T,Visit);
 	printf("\n后序遍历");
 	PostOrderTraverse(T,Visit);
+	
+	EnQueue(&LQ,*T);	
+
+	DeQueue(&LQ,&p);
+
+	printf("\n层序遍历");
+	LevelOrderTraverse(T,Visit);
 	
 	n=LeafNodeNum(T);
 	printf("\n叶子节点数 %d \n",n);
